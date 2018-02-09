4ab9dbb2a21494710b3021411c01dbbdc3bf415f
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-02-10 00:04:05 +0900

    remove app and create login

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Login from './login';
 import registerServiceWorker from './registerServiceWorker';
 import firebase from 'firebase';
 import { firebaseConfig } from './firebase-config.js';
 
 firebase.initializeApp(firebaseConfig);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Login />, document.getElementById('root'));
 registerServiceWorker();
--- src/login.js
+++ src/login.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './login.css';
 
-class App extends Component {
+class Login extends Component {
   render() {
     return (
       <div className="App">
@@ -14,4 +13,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Login;
