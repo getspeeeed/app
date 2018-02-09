7891d6533ef1b0c280652f1a38eb79ded6991e06
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-02-09 23:35:18 +0900

    complatible to firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCSFcHIN8XINl63cwo0AbYbUCPYNPSZZ_U",
+  authDomain: "tarostats.firebaseapp.com",
+  databaseURL: "https://tarostats.firebaseio.com",
+  projectId: "tarostats",
+  storageBucket: "tarostats.appspot.com",
+  messagingSenderId: "1031341491385"
+};
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
+import { firebaseConfig } from './firebase-config.js';
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const firebaseDb = firebaseApp.database();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
