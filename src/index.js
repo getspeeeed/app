2ccec771750dfffb41833a005b3931ea6b9c6367
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-02-10 12:40:14 +0900

    add material ui

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Login from './login';
 import registerServiceWorker from './registerServiceWorker';
 import firebase from 'firebase';
-import { firebaseConfig } from './firebase-config.js';
+import { slide as Menu } from 'react-burger-menu'
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import { firebaseConfig } from './firebase-config.js';
+import './index.css';
+import Login from './login';
+
+injectTapEventPlugin();
 firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<Login />, document.getElementById('root'));
