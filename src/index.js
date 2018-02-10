06648a33e6c7601ccbc37eea415b6155ce6ba301
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-02-10 23:50:26 +0900

    some fix

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import firebase from 'firebase';
-import { slide as Menu } from 'react-burger-menu'
 
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import { firebaseConfig } from './firebase-config.js';
 import './index.css';
+import App from './app';
 import Login from './login';
 
 injectTapEventPlugin();
 firebase.initializeApp(firebaseConfig);
-
-ReactDOM.render(<Login />, document.getElementById('root'));
 registerServiceWorker();
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    ReactDOM.render(<App />, document.getElementById('root'));
+  } else {
+    ReactDOM.render(<Login />, document.getElementById('root'));
+  }
+});
--- src/login.js
+++ src/login.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import './login.css';
 
+import theme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { RaisedButton, TextField } from 'material-ui';
+
+const style = {
+  color: "#ffffff",
+}
+
+const hint_style = {
+  color: "rgba( 255, 255, 255, 0.4 )",
+}
+
 class Login extends Component {
   render() {
     return (
-      <div className="App">
-        <input type="email" id="email" /><br />
-        <input type="password" id="password" /><br />
-        <input type="button" value="submit" />
-      </div>
+      <MuiThemeProvider muiTheme={getMuiTheme(theme)}>
+        <div className="login">
+          <div className="login-form">
+            <h1><img src="./img/logo-white.png" alt="Speeeed" className="logo" /></h1>
+            <TextField id="email" type="email" hintText="Email" fullWidth={true} inputStyle={ style } hintStyle={ hint_style } />
+            <TextField id="password" type="password" hintText="Password" fullWidth={true} inputStyle={ style } hintStyle={ hint_style } />
+            <RaisedButton className="signin" label="SIGN IN" primary={true} fullWidth={true} />
+            <p className="small center"><a href="#">Forgot your password?</a></p>
+          </div>
+          <div className="login-footer">
+            <RaisedButton className="signup" label="Create your account" secondary={true} style={ { margin: "1em 0" } } />
+          </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
